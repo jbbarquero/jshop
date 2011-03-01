b80a356500204831402e7c72226095f508d9510e
Javier Beneito Barquero <jbeneito@sermepa.es>
2011-03-01 10:40:20 +0100

    Third commit: Provider entity (without controller)

---
+++ src/test/java/com/malsolo/jshop/domain/ProviderDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.malsolo.jshop.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Provider.class)
+public class ProviderDataOnDemand {
+}
---
+++ src/test/java/com/malsolo/jshop/domain/ProviderIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.malsolo.jshop.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Provider.class)
+public class ProviderIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
